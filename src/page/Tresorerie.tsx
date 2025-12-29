24fc1ee2763359ce8af13ed2a397c54de7f7c75c
daniel_prince <danikoumba7@gmail.com>
2025-12-29 18:46:44 +0100

    prod

--- src/page/Tresorerie.tsx
+++ src/page/Tresorerie.tsx
@@ -114,12 +114,12 @@ async function tresor(){
                 {info.map((inf)=>(
                     <div className='flex justify-between gap-2 p-2 border border-black-500 rounded-lg ' key={inf.id}>
                         <p>{noms.includes(inf.nom)? ("Nature de l'operation"):('Nom') }: {inf.nom}</p>
-                        <p>PRIX : {inf.prix}</p>
+                        <p>PRIX : {inf.prix} fcfa</p>
                         <p>DATE : {inf.date}</p>
                         <p>Reste a payer : {inf.reste_a_payer}</p>
                         <p>type : {inf.type} {inf.type=='entree'?(<FaPlus color="white" className="bg-green-500 rounded-full p-1"/>):(<FaMinus color="white" className="bg-red-500 rounded-full p-1"/>)}</p>
-                        <p>Solde initial:{inf.si}</p>
-                        <p>Solde final:{inf.sf}</p>
+                        <p>Solde initial:{inf.si} fcfa</p>
+                        <p>Solde final:{inf.sf} fcfa</p>
                     </div>
                     
                 ))}
